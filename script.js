47fc050a427048e360167076b2fbbc16ea78e13b
Saceanu Ionut <saceanu.ionut@icloud.com>
2024-11-05 22:14:08 +0200

    add

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const logregBox = document.querySelector('.logreg-box');
 const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link'); 
+const registerLink = document.querySelector('.register-link');
 
 registerLink.addEventListener('click', () => {
-    logregBox.classList.add('active')
+    logregBox.classList.add('active');
 });
 
 loginLink.addEventListener('click', () => {
-    logregBox.classList.remove('active')
+    logregBox.classList.remove('active');
 });
